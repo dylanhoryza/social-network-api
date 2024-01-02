6067359f4822278746af56ec19af6c53324949a4
dylan horyza <horyza.dylan@gmail.com>
2024-01-02 13:18:16 -0700

    created all models

---
+++ models/Reaction.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require("mongoose");
+
+
+const reactionSchema = new Schema({
+  reactionId: {
+    type: mongoose.Schema.Types.ObjectId,
+    default: () => new mongoose.Types.ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxlength: 280,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: function (createdAt) {
+      return createdAt.toISOString(); 
+    },
+  },
+});
+
+module.exports = reactionSchema;
