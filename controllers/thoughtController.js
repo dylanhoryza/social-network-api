a9a9f375d90106d298b3ebf1787877ba9507ecd4
dylan horyza <horyza.dylan@gmail.com>
2024-01-03 14:55:29 -0700

    added reactions

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -70,4 +70,40 @@ module.exports = {
       res.status(500).json(err);
     }
   },
+    // Add a thought reaction
+    async addThoughtReaction(req, res) {
+      try {
+        const thought = await Thought.findOneAndUpdate(
+          { _id: req.params.thoughtId },
+          { $addToSet: { reactions: req.body } },
+          { runValidators: true, new: true }
+        );
+  
+        if (!thought) {
+          return res.status(404).json({ message: 'No thought with this id!' });
+        }
+  
+        res.json(thought);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    },
+    // Remove thought reaction
+    async removeThoughtReaction(req, res) {
+      try {
+        const thought = await Thought.findOneAndUpdate(
+          { _id: req.params.thoughtId },
+          { $pull: { reactions: { reactionId: req.params.reactionId } } },
+          { runValidators: true, new: true }
+        )
+  
+        if (!thought) {
+          return res.status(404).json({ message: 'No thought with this id!' });
+        }
+  
+        res.json(video);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    },
 };
